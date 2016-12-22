92e37bb962e3d93c737613d38eacbcc1d0b3f859
Oleksandr <cherednyk.o@gmail.com>
2016-12-22 17:31:33 +0200

    init commit

---
+++ book_bot/Controllers/MessagesController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace My_Bot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                if (activity != null && activity.Text != "carousel")
+                {
+                    await Conversation.SendAsync(activity, () => new EchoDialog());
+                }
+                else
+                {
+                    ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                    Activity replyToConversation = activity.CreateReply("Should go to conversation, with a carousel");
+                    replyToConversation.Recipient = activity.From;
+                    replyToConversation.Type = "message";
+                    replyToConversation.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+                    replyToConversation.Attachments = new List<Attachment>();
+                    Dictionary<string, string> cardContentList = new Dictionary<string, string>();
+                    cardContentList.Add("PigLatin", "http://wowslider.com/sliders/demo-34/data1/images/greatwilder1400498.jpg");
+                    cardContentList.Add("Pork Shoulder", "http://www.tokyoartbeat.com/media/event/2016/67A7-620");
+                    cardContentList.Add("Bacon", "http://www.visionlearning.com/blog/wp-content/uploads/2015/09/ezgif.com-optimize.gif");
+                    foreach (KeyValuePair<string, string> cardContent in cardContentList)
+                    {
+                        List<CardImage> cardImages = new List<CardImage>();
+                        cardImages.Add(new CardImage(url: cardContent.Value));
+                        List<CardAction> cardButtons = new List<CardAction>();
+                        CardAction plButton = new CardAction()
+                        {
+                            Value = $"https://en.wikipedia.org/wiki/{cardContent.Key}",
+                            Type = "openUrl",
+                            Title = "WikiPedia Page"
+                        };
+                        cardButtons.Add(plButton);
+                        HeroCard plCard = new HeroCard()
+                        {
+                            Title = $"I'm a hero card about {cardContent.Key}",
+                            Subtitle = $"{cardContent.Key} Wikipedia Page",
+                            Images = cardImages,
+                            Buttons = cardButtons
+                        };
+                        Attachment plAttachment = plCard.ToAttachment();
+                        replyToConversation.Attachments.Add(plAttachment);
+                    }
+                    replyToConversation.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+                    var reply = await connector.Conversations.SendToConversationAsync(replyToConversation);
+                }
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+
+    [Serializable]
+    public class EchoDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            await context.PostAsync("You said: " + message.Text);
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
